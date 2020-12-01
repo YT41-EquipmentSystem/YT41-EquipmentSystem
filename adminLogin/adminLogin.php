5b1ba37647c258b4c5056eb7e1afc32cd33b7517
asai <b7008@oic.jp>
2020-12-01 18:05:30 +0900

    [add] #7デザインの変更

--- adminLogin/adminLogin.php
+++ adminLogin/adminLogin.php
@@ -2,31 +2,51 @@
 
 session_start();
 
-if(isset($_SESSION['EMAIL'])){
+if (isset($_SESSION['EMAIL'])) {
   header('Location: ./top.html');
-　exit;
 }
 ?>
 
 <!DOCTYPE html>
 <html>
-  <head>
-    <link type="text/css" media="screen" href="./css/adminLogin.css" rel="stylesheet" />
-    <meta charset="utf-8" />
-    <title>管理者ログイン - 備品管理システム</title>
-    </head>
-  <body>
-    <form id ="loginInfo">
-    <p class="form-title">ログイン</p>
-    <p>メールアドレス</p>
-        <p id="mail"><input type="text" name="mail" /></p>
-    <p>パスワード</p>
-        <p id="pass"><input type="password" name="pass" /></p>
-      <p name="errormessage" id="errormessage"></p>
-      <div class="login_buttons">
-        <button type="button" id="login_button" name="send" value="ログイン" onclick="return false">ログイン</button></p>
-      </div>
-    </form>
-    <script type="text/javascript" src="./src/js/adminLogin.js"></script>
-  </body>
+
+<head>
+  <link type="text/css" media="screen" href="./css/adminLogin.css" rel="stylesheet" />
+  <meta charset="utf-8" />
+  <title>管理者ログイン - 備品管理システム</title>
+</head>
+
+<body>
+  <form id="loginInfo">
+    <h1>管理者ログイン</h1>
+    <hr>
+    <div align="center">
+      <table border="0">
+        <form action="list.html">
+          <tr>
+            <th class="login_field">
+              メールアドレス
+            </th>
+            <td class="login_field">
+              <input type="text" name="mail" id="mail">
+            </td>
+          </tr>
+          <tr>
+            <th class="login_field">
+              パスワード
+            </th>
+            <td class="login_field">
+              <input type="password" name="pass" id="pass">
+            </td>
+          </tr>
+          <p name="errormessage" id="errormessage"></p>
+          <tr>
+            <td colspan="2" class="login_button">
+              <button type="button" id="login_button" name="send" value="ログイン" onclick="return false">ログイン</button>
+            </td>
+          </tr>
+        </form>
+        <script type="text/javascript" src="./src/js/adminLogin.js"></script>
+</body>
+
 </html>
