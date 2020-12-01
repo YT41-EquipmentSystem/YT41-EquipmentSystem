21c0e572e987cc0f2d7386d0e6678d5a4728cb97
hiroto <shibaike.svaa134@gmail.com>
2020-12-01 16:37:25 +0900

    [add] #5 ドキュメントファイルの追加

--- adminLogin/adminLogin.php
+++ adminLogin/adminLogin.php
@@ -1,23 +1,10 @@
 <?php
+
 session_start();
 
-include_once('src/php/CSRFTOKEN.php');
-
-// ログイン済みの場合にセッション情報を破棄する
-if(isset($_SESSION['login'])){
-    // セッション変数を全て解除する
-    $_SESSION = array();
-    // セッションを切断するにはセッションクッキーも削除する。
-    // セッション情報だけでなくセッションを破壊する。
-    if (ini_get("session.use_cookies")) {
-        $params = session_get_cookie_params();
-        setcookie(session_name(), '', time() - 42000,
-            $params["path"], $params["domain"],
-            $params["secure"], $params["httponly"]
-        );
-    }
-    // 最終的に、セッションを破壊する
-    session_destroy();
+if(isset($_SESSION['EMAIL'])){
+  header('Location: ./top.html');
+　exit;
 }
 ?>
 
@@ -29,14 +16,17 @@ if(isset($_SESSION['login'])){
     <title>管理者ログイン - 備品管理システム</title>
     </head>
   <body>
-    <form id ="loginInfo" action="./src/php/db/login.php" method="post">
-      <input type="hidden" name="token" id="token" value="<?=CsrfValidator::generate()?>">
-      <p>メールアドレス： <input type="text" name="mail" id="mail"></p>
-      <p>パスワード： <input type="password" name="pass" id="pass"></p>
+    <form id ="loginInfo">
+    <p class="form-title">ログイン</p>
+    <p>メールアドレス</p>
+        <p id="mail"><input type="text" name="mail" /></p>
+    <p>パスワード</p>
+        <p id="pass"><input type="password" name="pass" /></p>
       <p name="errormessage" id="errormessage"></p>
       <div class="login_buttons">
-        <button type="submit">ログイン</button></p>
+        <button type="button" id="login_button" name="send" value="ログイン" onclick="return false">ログイン</button></p>
       </div>
     </form>
+    <script type="text/javascript" src="./src/js/adminLogin.js"></script>
   </body>
 </html>
--- adminLogin/src/php/db/login.php
+++ adminLogin/src/php/db/login.php
@@ -1,62 +1,40 @@
 <?php
-//================
-//　
-//================
-require_once('../../../adminLogin.php');
-require_once('../DBCONNECTION.php');
-
 // ログイン画面で入力したメールアドレスとパスワードを取得
-$mail = $_POST['mail'];//filter_input(INPUT_POST, mail, FILTER_VALIDATE_EMAIL);
-$password = $_POST['pass'];//filter_input(INPUT_POST, password, FILTER_VALIDATE_EMAIL);
+$mail = isset($_POST['mail']) ? $_POST['mail'] : null;
+$password = isset($_POST['pass']) ? $_POST['pass'] : null;
 
 try {
-    // データベースに接続
-    $pdo = DbConnection::dbconnect();
 
-    // メールアドレスが英数文字,[@],[.]のみであるか確認
     if(!preg_match("/^[a-zA-Z0-9@.]+$/" , $mail)){
-        exit;// catchさせるべき
+        exit('2');
     }
 
     // サニタイジング(XSS対策)
     htmlspecialchars($mail, ENT_QUOTES, 'UTF-8');
     htmlspecialchars($password, ENT_QUOTES, 'UTF-8');
 
-    // メールアドレスに一致するデータを取得(パスワードはハッシュ化されているため後でチェックする)
-    $sql  = 'SELECT *';
-    $sql .= ' FROM administrator';
-    $sql .= ' WHERE id = :id';
+    // データベースに接続
+    $pdo = new PDO(
+        'mysql:dbname=equipmentsystem;host=localhost;charset=utf8mb4',
+        'test',
+        'test',
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        ]
+    );
 
-    $stmt = $pdo->prepare($sql);
+    $stmt = $pdo->prepare('SELECT count(*) FROM administrator WHERE id = :id AND password = :password');
     $stmt->bindValue(':id', $mail);
+    $stmt->bindValue(':password', $password);
     $stmt->execute();
 
-    // カラム名をキーとする連想配列で取得
-    $id = $stmt->fetch(PDO::FETCH_ASSOC);
-    // idチェック
-    if(!$id){
-        echo 'ユーザ名かパスワードが正しくありません。';// 出力場所を選ぶべき
-        exit;//catchさせるべき
+    while (false !== $value = $stmt->fetchColumn()) {
+        if($value == 1){
+            $_SESSION['EMAIL'] = $mail;
+        }
+        echo $value;
     }
-
-    //パスワードチェック
-    if(!password_verify($password, $id['password'])){
-        echo 'ユーザ名かパスワードが正しくありません。';// 出力場所を選ぶべき
-        exit;//catchさせるべき
-    }
-
-    // 更新の必要があるかチェック
-    if(password_needs_rehash($id['password'], PASSWORD_ARGON2I)){
-        // hashアルゴリズムが古い場合に更新する
-        $sql = 'UPDATE administrator SET password = :password WHERE name = :name';
-        $stmt = $pdo->prepare($sql);
-        $stmt->bindValue(':id', $mail);
-        $stmt->bindValue(':password', password_hash($password, PASSWORD_ARGON2I));
-        $stmt->execute();
-    }
-
-    // CSLF対策のトークンをtopへ引き継いでリダイレクト
-    header('Location: ../../../top.php', true, 307);
 
 } catch (PDOException $e) {
     // エラーが発生した場合は「500 Internal Server Error」でテキストとして表示して終了する
