098dc2168814b02399530b7cc487210c128f8a33
hiroto <shibaike.svaa134@gmail.com>
2020-12-01 16:37:25 +0900

    [add] #5 ドキュメントファイルの追加

--- adminLogin/top.php
+++ adminLogin/top.php
@@ -1,40 +1,20 @@
 <?php
-//===============================
-//ログイン後のページ(仮)
-//===============================
-session_start();
-// アクセストークンを取得する関数を使えるようにする
-include_once("src/php/CSRFTOKEN.php");
-
-try {
-    // トップページにリダイレクトした際に行う処理
-    if(!isset($_SESSION['login'])){
-        CsrfValidator::validate(filter_input(INPUT_POST, 'token'), true);
-        // ログイン時にはセッションIDを更新する
-        session_regenerate_id(true);
-        // ログイン成功情報を保存
-        $_SESSION['login'] = true;
-    }
-    echo 'ようこそ';
-} catch (\RuntimeException $e) {
-    // コメントアウト文はログ出力させる
-    //header('Content-Type: text/plain; charset=UTF-8', true, $e->getCode() ?: 500);
-    //die($e->getMessage());
-
-    // エラー時は管理者ログイン画面にリダイレクト
-    header('Location: adminLogin.php');
-}
+  session_start();
+  if(isset($_SESSION['EMAIL'])){
+    echo 'ようこそ' .  $_SESSION['EMAIL'] . "さん<br>";
+  }
 ?>
+
 
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8" />
-        <title>トップ - 備品管理システム</title>
-    </head>
-    <body>
-        <form id ="loginInfo">
-            <p></p>
-        </form>
-    </body>
+<head>
+<meta charset="utf-8" />
+<title>トップ - 備品管理システム</title>
+</head>
+<body>
+<form id ="loginInfo">
+  <p>ログイン成功</p>
+</form>
+</body>
 </html>
